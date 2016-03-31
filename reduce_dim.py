4b6444abfa4893a3f4d5f76436f44f3fe00e360a
asierrayk <asier314@gmail.com>
2016-03-31 15:22:15 +0200

    plantilla de reduce_dim

---
+++ reduce_dim.py
@@ -0,0 +1,19 @@
+from __future__ import division
+import numpy as np 
+from scipy.linalg import eigh 
+
+class LDA:
+    
+    def fit(self, X_train, y_train, reduced_dim):
+        pass #type your code here
+
+    def transform(self, X):
+        pass #type your code here
+
+class PCA:
+    
+    def fit(self, X_train, reduced_dim):
+        pass #type your code here
+
+    def transform(self, X):
+        pass #type your code here
