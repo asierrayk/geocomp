f30191c108ba69e380bec108fa5108eb12f0ccd0
asierrayk <asier314@gmail.com>
2016-03-31 18:27:52 +0200

    no funciona practica 2

--- reduce_dim.py
+++ reduce_dim.py
@@ -10,21 +10,16 @@ class LDA:
         self.mean_train = None
     
     def fit(self, X_train, y_train, reduced_dim):
-        pass #type your code here
-        X0 = np.asarray([[1,2],[1,1]])
-        X1 = np.asarray([[3,2],[4,7]])
-        
-        X_train = np.asarray([[1,2],[3,2],[4,7],[1,1]])
-        y_train = np.asarray([0,1,1,0])
-        
+    
         self.mean_train = np.mean(X_train, axis=0)                
         
         K = max(y_train) + 1
         N, D = X_train.shape
         S_w = np.zeros((D,D))
         S_b = np.zeros((D,D))
-        for k in [0,K]:
-            X_k = [X_train[i] for i in range (0,N) if y_train[i]==k]
+        for k in range(0,K):
+            X_k = np.asarray([X_train[i] for i in range(0,N) if y_train[i]==k])
+            #print(X_k)
             mean_k = np.mean(X_k, axis=0)                
             
             S_w += np.cov(X_k, rowvar=0, bias=1) * X_k.shape[0]
@@ -35,7 +30,10 @@ class LDA:
         self.W_LDA = np.take(eigh_vectors.T, range(D-reduced_dim, D), axis=0)
         
     def transform(self, X):
-        return (X-self.mean_train).dot(self.W_LDA.T)
+        #print "X ", X
+        #print "mean_t ", self.mean_train
+        #print "W ", self.W_LDA
+        return (X - self.mean_train).dot(self.W_LDA.T)
             
 class PCA:
 
