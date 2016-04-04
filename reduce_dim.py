c260712db0471069ff96f57c6fc4809f8969a66d
Aryalexa <aryalexa@gmail.com>
2016-04-04 18:34:43 +0200

    Solución casi igual
    borrar comentarios

--- reduce_dim.py
+++ reduce_dim.py
@@ -15,21 +15,41 @@ class LDA:
         
         K = max(y_train) + 1
         N, D = X_train.shape
+        #print "N:", N, "D:", D, "K:", K, "D':",reduced_dim
         S_w = np.zeros((D,D))
         S_b = np.zeros((D,D))
         for k in range(0,K):
+            #print "clase --------------------------------------------", k
+            
             X_k = np.asarray([X_train[i] for i in range(0,N) if y_train[i]==k])
-            #print(X_k)
-            mean_k = np.mean(X_k, axis=0)                
+            #print "X_k ", X_k
+            mean_k = np.mean(X_k, axis=0) 
+            N_k = X_k.shape[0]
             
-            S_w += np.cov(X_k, rowvar=0, bias=1) * X_k.shape[0]
+            # S within
+            for x in X_k:
+                aux5 = np.outer((x - mean_k),(x - mean_k))
+                S_w += aux5
+            #S_w += np.cov(X_k, rowvar=0, bias=1) * N_k
             
-            aux = X_k.shape[0] * np.outer((mean_k - self.mean_train.T),(mean_k - self.mean_train))
+            # S between
+            aux = N_k * np.outer((mean_k - self.mean_train),(mean_k - self.mean_train))
             #print "Sb_k ",aux
             S_b += aux            
             
-        eigh_values, eigh_vectors = eigh(np.linalg.solve(S_w, S_b))
-        self.W_LDA = np.take(eigh_vectors.T, range(D-reduced_dim, D), axis=0)
+        #aux3 = np.linalg.solve(S_w, S_b)
+        #aux4 = np.linalg.inv(S_w) * S_b
+        #print "Sw, Sb"
+        #print S_w
+        #print S_b
+        #print "matriz nada",
+        eigh_values, eigh_vectors = eigh(S_b, S_w) #aux4
+        #print "autoval ", eigh_values
+        #print "autovec ", eigh_vectors
+        aux2 = np.take(eigh_vectors.T, range(D-1, D-reduced_dim-1, -1), axis=0)
+        aux22 = np.take(eigh_vectors.T, range(D-reduced_dim,D), axis=0)
+        #print "sol: ", aux2 , aux22
+        self.W_LDA = aux22
         
     def transform(self, X):
         #print "X ", X
@@ -49,7 +69,7 @@ class PCA:
         
         S_t = np.cov(X_train, rowvar=0, bias=1)
         eigh_values, eigh_vectors = eigh(S_t)
-        self.W_PCA = np.take(eigh_vectors.T, range(D-reduced_dim, D), axis=0)        
+        self.W_PCA = np.take(eigh_vectors.T, range(D-reduced_dim, D), axis=0)     
             
     def transform(self, X):
         return (X-self.mean_train).dot(self.W_PCA.T)
--- reduce_dim_test.py
+++ reduce_dim_test.py
@@ -6,11 +6,27 @@ from reduce_dim import LDA, PCA
 #X0 = np.asarray([[1,2],[1,1]])
 #X1 = np.asarray([[3,2],[4,7]])
         
+        
+from sklearn import datasets
+
+
+db = datasets.load_digits()
+#X_train, y_train = db.data, db.target     
+#X_test = X_train   
+        
+        
 X_train = np.asarray([[1,2,3],[3,2,5],[4,7,7],[1,1,6]])
 y_train = np.asarray([0,1,1,0])
 X_test = np.asarray([[1,2,5],[3,2,7],[4,7,3],[1,1,9],[2,3,3]])
 
+print "X train ", X_train
+print "y train ", y_train
 L = LDA()
 L.fit(X_train, y_train, 2)
 rt = L.transform(X_test)        
 print(rt)
+
+P = PCA()
+P.fit(X_train, 2)
+rt2 = P.transform(X_test)
+print(rt2)
