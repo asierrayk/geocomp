86e715960866882fdb1f5a98ac747452981fd296
Aryalexa <aryalexa@gmail.com>
2016-05-26 01:51:41 +0200

    graham alg almost finished

--- Opcional/ConvexHull.py
+++ Opcional/ConvexHull.py
@@ -1,31 +1,63 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from Point import Point
 
 class ConvexHull:
-    
-    def graham(self, Points):
+
+    def graham(self, points):
+        if not isinstance(points, list):
+            return
+            
+        points.sort()
+        n = len(points)
+        P = self.fromListToPoints(points, n)
+        '''
         P = np.asarray(Points)
-        P = np.sort(P)
-        n = P.shape[0]        
+        n = P.shape[0]
+        P = np.sort(P, )
+        P = self.fromListToPoints(P, n)
+        '''
+
+        #print 'puntos', P
         
-        L_upper = P[:2]
-        
-        for i in xrange(3, n):
+        L_upper = P[:2] # first two elem
+        for i in xrange(2, n):
             last = P[i]
-            while L_upper.shape[0] >= 2 and  last.isLeft(P[-2, -1]):
+            while len(L_upper) >= 2 and last.isLeft(P[-2], P[-1]):
                 L_upper.pop()
             L_upper.append(last)
-            
+
         L_lower = P[-2:]
-        
-        
         for i in range(n-3, -1, -1):
             last = P[i]
-            while L_lower.shape[0] >= 2 and  last.isLeft(P[-2, -1]):
+            while len(L_lower) >= 2 and last.isLeft(P[-2], P[-1]):
                 L_lower.pop()
             L_lower.append(last)
-            
-        return np.vstack(L_upper.pop(), L_lower.pop())
-                
-            
-            
+
+        return L_upper.pop() + L_lower.pop()
+
+    def fromListToPoints(self, p, n):
+        p2 = [Point(p[0][0], p[0][1])]
+        for i in range(1, n):
+            p2.append(Point(p[i][0], p[i][1]))
+        return p2
+
+
+#         E X A M P L E
+
+C = ConvexHull()
+p = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]]
+#r = C.graham(p)
+
+# .. paso a paso:
+
+p.sort()
+n = len(p)
+P = C.fromListToPoints(p, n)
+
+L_upper = P[:2]
+# i = 2
+last = P[2]
+b = last.isLeft(P[-2], P[-1]) # esto da false !
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # geocomp
+
+To modify a Python code to conform to the PEP 8 style guide:
+
+`$ autopep8 --in-place --aggressive --aggressive <filename>`
